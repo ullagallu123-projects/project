10195df8876c26134357a874665cd37e55831507
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-01-29 12:38:09 +0530

    add the frontend

--- expense-project/qa/06.backend-frontend/data.tf
+++ expense-project/qa/06.backend-frontend/data.tf
@@ -16,6 +16,28 @@ data "aws_ami" "backend" {
     values = ["ebs"]
   }
 }
+data "aws_ami" "frontend" {
+  most_recent = true
+  owners      = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["frontend-*"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+data "aws_ssm_parameter" "frontend" {
+  name  = "/${var.environment}/${var.project_name}/frontend-sg"
+}
 
 data "aws_ssm_parameter" "backend" {
   name  = "/${var.environment}/${var.project_name}/backend-sg"
@@ -31,4 +53,7 @@ data "aws_ssm_parameter" "private_subnet_ids" {
 
 data "aws_ssm_parameter" "internal_http_listner" {
   name  = "/${var.environment}/${var.project_name}/internal-http-listner"
+}
+data "aws_ssm_parameter" "external_https_listner" {
+  name  = "/${var.environment}/${var.project_name}/external-https-listner"
 }
--- expense-project/qa/06.backend-frontend/main.tf
+++ expense-project/qa/06.backend-frontend/main.tf
@@ -27,3 +27,34 @@ module "backend_asg"{
     component = "internal"
     zone_name = var.zone_name
 }
+
+module "frontend_asg"{
+    source = "../../../module/expesne-asg"
+    environment = var.environment
+    project_name = var.project_name
+    common_tags = {
+    "Terraform"   = "true"
+    "Author"      = "sivaramakrishna"
+    "Environment" = "qa"
+    "Project"     = "expense"
+    "Component"   = "frontend-asg"
+  }
+    ami_id = data.aws_ami.frontend.id
+    instance_type = var.instance_type
+    key_name = var.key_name
+    vpc_security_group_ids = [data.aws_ssm_parameter.frontend.value]
+    port = 80
+    health_check_path = "/health"
+    vpc_id = data.aws_ssm_parameter.vpc_id.value
+    asg_max_size = 1
+    asg_min_size = 1
+    desired_capacity = 1
+    health_check_grace_period  = 60
+    subnets = split(",",data.aws_ssm_parameter.private_subnet_ids.value)
+
+    target_value = 10
+    listener_arn = data.aws_ssm_parameter.external_https_listner.value
+    component = "external"
+    zone_name = var.zone_name
+
+}
